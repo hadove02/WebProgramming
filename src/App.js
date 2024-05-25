13d591c4e34a4f856c0644b8a3b5702cd8ecaeb6
hadove02 <hadove02@naver.com>
2024-05-26 03:01:03 +0900

    서버 및 DB 연동, 테스트

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+function App() {
+
+    const [data, setData] = useState([{
+        userNo:1,
+        id: "",
+        pw: "",
+        userType: null
+    }]);
+
+    const test = () => {
+        axios.get('http://localhost:5000/test').then(res => {
+            setData(res.data);
+        })
+    }
+
+    return (
+        <div>
+            <button onClick={test}>getId</button>
+            <br/>
+            userNo : {data[0].userNo}, id : {data[0].id}, pw : {data[0].pw}, userType: {data[0].userType}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/DB.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql2');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port= 5000;
+
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'asdf15963',
+    database: 'webproject'
+})
+
+conn.connect((err) => {
+    if (err) console.log(err);
+    else console.log('Connect');
+})
+
+app.use(cors());
+
+app.get("/test", (req, res) => {
+    conn.query("SELECT * FROM login", (err, result) => {
+        if(err){
+            console.log(err);
+        } else{
+            res.send(result);
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log("start");
+})
+
+
+module.exports = conn;
